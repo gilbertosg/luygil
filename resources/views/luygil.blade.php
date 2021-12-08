42e0a73823c1d18c622588b2e3743f04622aa848
Silva <gilgon@38f9d3597234.ant.amazon.com>
2021-12-08 09:20:18 -0800

    first commit

---
+++ resources/views/luygil.blade.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en" dir="">
+<head>
+  <!-- Required Meta Tags Always Come First -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Title -->
+  <title>Lu y Gil</title>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="./favicon.ico">
+
+  <!-- Font -->
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
+
+  <!-- CSS Implementing Plugins -->
+  <link rel="stylesheet" href="./assets/vendor/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="./assets/vendor/hs-mega-menu/dist/hs-mega-menu.min.css">
+
+  <!-- CSS Unify Template -->
+  <link rel="stylesheet" href="./assets/css/theme.min.css">
+</head>
+
+<body>
+
+  <!-- ========== HEADER ========== -->
+  <header id="header" class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container">
+      <nav class="js-mega-menu navbar-nav-wrap">
+        <!-- Default Logo -->
+        <a class="navbar-brand" href="./index.html" aria-label="Unify">
+          <img class="navbar-brand-logo" src="./assets/img/logo_lg.png" alt="Image Description">
+        </a>
+        <!-- End Default Logo -->
+
+        <!-- Toggler -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-default">
+            <i class="bi-list"></i>
+          </span>
+          <span class="navbar-toggler-toggled">
+            <i class="bi-x"></i>
+          </span>
+        </button>
+        <!-- End Toggler -->
+
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav nav-pills">
+
+            <!-- Log in -->
+            <li class="nav-item ms-lg-auto">
+              <a class="btn btn-ghost-dark me-2 me-lg-0" href="#">Te Amo</a>
+              <a class="btn btn-dark d-lg-none" href="./page-signup.html">A</a>
+            </li>
+            <!-- End Log in -->
+
+          </ul>
+        </div>
+        <!-- End Collapse -->
+      </nav>
+    </div>
+  </header>
+
+  <!-- ========== END HEADER ========== -->
+
+
+  <!-- ========== MAIN CONTENT ========== -->
+  <main id="content" role="main">
+    <!-- User Profile -->
+    <div class="container content-space-2">
+      <div class="row justify-content-md-center">
+        <div class="col-md-8">
+          <div class="text-center">
+            <img class="avatar avatar-xxl avatar-circle mb-3" src="./assets/img/lg.png" alt="Image Description">
+
+            <div class="mb-4">
+              <h3>Lu y Gil</h3>
+              <span class="d-block mb-1">Nuestra historia con el paso del tiempo,</span>
+              <p>desde cómo empezamos hasta finales de noviembre... continuará!</p>
+            </div>
+          </div>
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End User Profile -->
+
+    <!-- Card Grid -->
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div class="col-md-8 content-space-b-2">
+          <div class="d-grid">
+            <!-- Card -->
+            <a class="card card-ghost card-transition-zoom" data-fslightbox="youtube-video" href="https://www.youtube.com/watch?v=RJtrzPzodP8">
+              <div class="card-transition-zoom-item">
+                <img class="card-img" src="./assets/img/1920x1080/abril_lg.png" alt="Image Description">
+              </div>
+
+              <div class="card-body">
+                <h4>Abril 2021</h4>
+                <p class="card-text">Primera cita en Bellevue y nuestro primer beso.</p>
+              </div>
+            </a>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <a class="card card-ghost card-transition-zoom" data-fslightbox="youtube-video" href="https://www.youtube.com/watch?v=RJtrzPzodP8">
+              <div class="card-transition-zoom-item">
+                <img class="card-img" src="./assets/img/1920x1080/mayo_lg.png" alt="Image Description">
+              </div>
+
+              <div class="card-body">
+                <h4>Mayo 2021</h4>
+                <p class="card-text">Bainbridge.</p>
+              </div>
+            </a>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <a class="card card-ghost card-transition-zoom" data-fslightbox="youtube-video" href="https://www.youtube.com/watch?v=RJtrzPzodP8">
+              <div class="card-transition-zoom-item">
+                <img class="card-img" src="./assets/img/1920x1080/junio_lg.png" alt="Image Description">
+              </div>
+
+              <div class="card-body">
+                <h4>Junio 2021</h4>
+                <p class="card-text">...</p>
+              </div>
+            </a>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <a class="card card-ghost card-transition-zoom" data-fslightbox="youtube-video" href="https://www.youtube.com/watch?v=RJtrzPzodP8">
+              <div class="card-transition-zoom-item">
+                <img class="card-img" src="./assets/img/1920x1080/julio_lg.png" alt="Image Description">
+              </div>
+
+              <div class="card-body">
+                <h4>Julio 2021</h4>
+                <p class="card-text">...</p>
+              </div>
+            </a>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <a class="card card-ghost card-transition-zoom" data-fslightbox="youtube-video" href="https://www.youtube.com/watch?v=RJtrzPzodP8">
+              <div class="card-transition-zoom-item">
+                <img class="card-img" src="./assets/img/1920x1080/agosto_lg.png" alt="Image Description">
+              </div>
+
+              <div class="card-body">
+                <h4>Agosto 2021</h4>
+                <p class="card-text">...</p>
+              </div>
+            </a>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <a class="card card-ghost card-transition-zoom" data-fslightbox="youtube-video" href="https://www.youtube.com/watch?v=RJtrzPzodP8">
+              <div class="card-transition-zoom-item">
+                <img class="card-img" src="./assets/img/1920x1080/septiembre_lg.png" alt="Image Description">
+              </div>
+
+              <div class="card-body">
+                <h4>Septiembre 2021</h4>
+                <p class="card-text">...</p>
+              </div>
+            </a>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <a class="card card-ghost card-transition-zoom" data-fslightbox="youtube-video" href="https://www.youtube.com/watch?v=RJtrzPzodP8">
+              <div class="card-transition-zoom-item">
+                <img class="card-img" src="./assets/img/1920x1080/octubre_lg.png" alt="Image Description">
+              </div>
+
+              <div class="card-body">
+                <h4>Octubre 2021</h4>
+                <p class="card-text">...</p>
+              </div>
+            </a>
+            <!-- End Card -->
+
+            <!-- Card -->
+            <a class="card card-ghost card-transition-zoom" data-fslightbox="youtube-video" href="https://www.youtube.com/watch?v=fLhjgGUxNoQ">
+              <div class="card-transition-zoom-item">
+                <img class="card-img" src="./assets/img/1920x1080/noviembre_lg.png" alt="Image Description">
+              </div>
+
+              <div class="card-body">
+                <h4>Noviembre 2021</h4>
+                <p class="card-text">...</p>
+              </div>
+            </a>
+            <!-- End Card -->
+          </div>
+
+
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End Card Grid -->
+
+  </main>
+  <!-- ========== END MAIN CONTENT ========== -->
+
+  <!-- ========== SECONDARY CONTENTS ========== -->
+  <!-- Go To -->
+  <a class="js-go-to go-to position-fixed" href="javascript:;" style="visibility: hidden;"
+     data-hs-go-to-options='{
+       "offsetTop": 700,
+       "position": {
+         "init": {
+           "right": "2rem"
+         },
+         "show": {
+           "bottom": "2rem"
+         },
+         "hide": {
+           "bottom": "-2rem"
+         }
+       }
+     }'>
+    <i class="bi-chevron-up"></i>
+  </a>
+  <!-- ========== END SECONDARY CONTENTS ========== -->
+
+  <!-- JS Global Compulsory  -->
+  <script src="./assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+  <!-- JS Implementing Plugins -->
+  <script src="./assets/vendor/hs-header/dist/hs-header.min.js"></script>
+  <script src="./assets/vendor/hs-mega-menu/dist/hs-mega-menu.min.js"></script>
+  <script src="./assets/vendor/prism/prism.js"></script>
+  <script src="./assets/vendor/fslightbox/index.js"></script>
+  <script src="./assets/vendor/hs-go-to/dist/hs-go-to.min.js"></script>
+
+  <!-- JS Unify -->
+  <script src="./assets/js/theme.min.js"></script>
+
+  <!-- JS Plugins Init. -->
+  <script>
+    (function() {
+      // INITIALIZATION OF NAVBAR
+      // =======================================================
+      new HSHeader('#header').init()
+
+
+      // INITIALIZATION OF MEGA MENU
+      // =======================================================
+      const megaMenu = new HSMegaMenu('.js-mega-menu', {
+        desktop: {
+          position: 'left'
+        }
+      })
+
+
+      // INITIALIZATION OF GO TO
+      // =======================================================
+      new HSGoTo('.js-go-to')
+    })()
+  </script>
+</body>
+</html>
